39b9b611a72c21d8bc96d40b8e9301762f371a5a
Vanshika <89936851+Vanshika0421@users.noreply.github.com>
2023-04-01 10:52:54 +0530

    commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -22,9 +22,25 @@ export const Home = () => {
   <GrLocation className="icon"/>
 </div>
         </div>
+        <div className='dateInput'>
+          <label htmlFor='date'>Enter Date</label>
+<div className='input flex'>
+  <input type="date"/>    
       </div>
+    </div>
+    <div className='priceInput'>
+      <div className='label_total flex'>
+          <label htmlFor='price'>Price</label>
+          <h3 className='total'>$5000</h3>
+          </div>
+<div className='input flex'>
+  <input type="range" max="5000"  min="1000" />
+    </div>
+    </div>
+    </div>
     </div>
    </section>
   )
-}
+  }
+
 export default Home;
